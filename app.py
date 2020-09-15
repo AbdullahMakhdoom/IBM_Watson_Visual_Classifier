6d455835f2f16919eed418ec5be2cc5bc1d7d16b
AbdullahMakhdoom <30556653+AbdullahMakhdoom@users.noreply.github.com>
2020-09-15 14:26:32 +0500

    Main Flask App

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-import os, shutils
+import os
 from flask import Flask, request, redirect, url_for, render_template, send_from_directory
 from werkzeug.utils import secure_filename
-from pandas.io.json import json_normalize
-from zipfile import ZipFile
+import zipfile
+import shutil
 
 
-classifier_id = 'furnitureclassifier_1414993083'
 my_apikey = 'kjiT3WJh3p989an0djT_BxwaOWeP-NsP8GSHjiSxTdft'
 
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
@@ -16,23 +15,13 @@ app = Flask(__name__, static_url_path="/static")
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+# limit upload size upto 8mb
+# app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
 
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
-def getdf_visrec(url, classifier_ids, apikey = my_apikey):
-
-    json_result = visrec.classify(url=url,
-                              threshold='0.6',
-                              classifier_ids=classifier_ids).get_result()
-
-    json_classes = json_result['images'][0]['classifiers'][0]['classes']
-
-    df = json_normalize(json_classes).sort_values('score', ascending=False).reset_index(drop=True)
-
-    return df
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -41,32 +30,56 @@ def index():
             print('No file attached in request')
             return redirect(request.url)
         file = request.files['file']
-        print(file)
         if file.filename == '':
             print('No file selected')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            print(filename)
-            with ZipFile(filename,'r') as zipImg:
-                zipImg.extractall()
-            for roots, dirs, img in os.walk(UPLOAD_FOLDER, topdown=False ):
-                process_file(roots, img)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('uploaded_file', filename=filename))
     return render_template('index.html')
 
 
-def process_file(path, filename):
-    result_df = getdf_visrec(url = filename, classifier_ids= classifier_id)
-    class_dir = result_df["class"]
-    if not os.path.exists(os.path.join(app.config['DOWNLOAD_FOLDER'], classdir)):
-        os.mkdir(os.path.join(app.config['DOWNLOAD_FOLDER'], classdir))
-    shutil.move(path, os.path.join(app.config['DOWNLOAD_FOLDER'], classdir))
+def process_zip(path, filename):
+    # Unzip the fashion testing in downloads folder
+    zip_ref = zipfile.ZipFile(os.path.join(UPLOAD_FOLDER, filename), 'r')
+    zip_ref.extractall(DOWNLOAD_FOLDER)
+    zip_ref.close()
+
+    # Name of the testing folder i.e: Testfashion
+    testing_folder = filename.rsplit('.', 1)[0]
+    print(testing_folder)
+
+    if not os.path.exists(DOWNLOAD_FOLDER + 'Classified'):
+        print('Classified folder made')
+        os.mkdir(DOWNLOAD_FOLDER + 'Classified')
+
+    # classifying images in testing folder
+    for (roots, dirs, images) in os.walk(os.path.join(DOWNLOAD_FOLDER , testing_folder)):
+        for img in images:
+            # Classifier function
+            # class_dir = predict_df["class"]
+
+            class_dir = 'longdress' # dummy variable
+            print("reading images...")
+
+            # If folder for predicted class does not exist
+            if not os.path.exists(DOWNLOAD_FOLDER + 'Classified/' + class_dir):
+                os.mkdir(DOWNLOAD_FOLDER + 'Classified/' + class_dir) # then make directory
+                print("directory made")
+
+            # Moving the image from testing folder to predicted class folder
+            print(img)
+            source = os.path.join(DOWNLOAD_FOLDER , testing_folder, img)
+            destination = os.path.join(DOWNLOAD_FOLDER ,'Classified' ,class_dir)
+            shutil.move(source , destination)
+    os.rmdir(os.path.join(DOWNLOAD_FOLDER , testing_folder))
+    shutil.make_archive(DOWNLOAD_FOLDER+'Classified', 'zip', DOWNLOAD_FOLDER + 'Classified')
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename, as_attachment=True)
-
+    process_zip(os.path.join(app.config['UPLOAD_FOLDER'], filename), filename)
+    return send_from_directory(app.config['DOWNLOAD_FOLDER'], 'Classified.zip', as_attachment=True)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
